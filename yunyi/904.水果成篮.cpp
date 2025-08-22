1c09338f3fc167776cc6285c46a55b5cfb231aa5
nemosunny <1150461187@qq.com>
2025-08-22 18:42:21 +0800

    20250822

---
+++ yunyi/904.水果成篮.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=904 lang=cpp
+ *
+ * [904] 水果成篮
+ * 问题本质是找只包含2种数字的最长子数组
+ */
+
+// @lc code=start
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        if (fruits.empty()) return 0;
+        unordered_map<int, int> basket;
+        int start = 0;
+        int results = 0;
+        for (int end=0;end<fruits.size();end++) {
+            basket[fruits[end]]++; //通过计数的方式将水果放入篮子
+            while (basket.size() > 2) {
+                int left_fruit = fruits[start];
+                basket[left_fruit]--;
+                if (basket[left_fruit] == 0) {
+                    basket.erase(left_fruit);
+                }
+                start++;
+            }
+            results = max(results, end-start+1);
+        }
+        return results;
+    }
+};
+// @lc code=end
+
